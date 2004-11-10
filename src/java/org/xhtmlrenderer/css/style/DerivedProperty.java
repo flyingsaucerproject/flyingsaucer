7190e917910b255ad4845ac72a58ba6577430b04
tobega <tobega>
2004-11-10 04:26:55 +0000

    no message

--- src/java/org/xhtmlrenderer/css/style/DerivedProperty.java
+++ src/java/org/xhtmlrenderer/css/style/DerivedProperty.java
@@ -1,7 +1,7 @@
 /*
  *
  * DerivedProperty.java
- * Copyright (c) 2004 Torbjörn Gannholm
+ * Copyright (c) 2004 Patrick Wright, Torbjörn Gannholm
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
