40c742265778f3cb04f903fa62e12050f9d2a7b2
tobega <tobega>
2004-12-12 02:23:34 +0000

    Making good progress on render. Still broken, won't even compile at the moment. Working hard to fix it, though.

--- src/java/org/xhtmlrenderer/render/CustomBlockRenderer.java
+++ src/java/org/xhtmlrenderer/render/CustomBlockRenderer.java
@@ -1,20 +1,20 @@
 package org.xhtmlrenderer.render;
 
 
-import org.xhtmlrenderer.layout.*;
-import org.xhtmlrenderer.util.u;
+import org.xhtmlrenderer.layout.Context;
+import org.xhtmlrenderer.util.U;
 
 public class CustomBlockRenderer extends BoxRenderer {
     /**
      * override this to paint your component
      *
-     * @param c    PARAM
-     * @param box  PARAM
+     * @param c   PARAM
+     * @param box PARAM
      */
-    public void paintComponent( Context c, Box box ) {
-        u.p( "Custom components must override paintComponent" );
+    public void paintComponent(Context c, Box box) {
+        U.p("Custom components must override paintComponent");
     }
-    
-    
+
+
 }
 
