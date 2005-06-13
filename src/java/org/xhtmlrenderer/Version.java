b5113fd838499ccf6ec51c403d7282abcec5bf42
pdoubleya <pdoubleya>
2005-06-13 08:47:33 +0000

    Added to CVS.

---
+++ src/java/org/xhtmlrenderer/Version.java
@@ -0,0 +1,33 @@
+/*
+ * {{{ header & license
+ * Copyright (c) 2004, 2005 Patrick Wright
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * }}}
+ */
+
+package org.xhtmlrenderer;
+
+/**
+ * A class to keep track of build versions of Flying Saucer releases.
+ * Currently maintained manually. Inspired by Version in http://huckster.dev.java.net
+ *
+ * @author Patrick Wright
+ */
+public class Version {
+    static final String version="R5pre1";
+    static final String buildDay = "June 12 2005";
+    static final String buildNumber = "0";
+}
