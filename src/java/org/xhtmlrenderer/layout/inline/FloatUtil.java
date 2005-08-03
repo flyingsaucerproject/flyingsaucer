bde92034552ad767004ee99ea3102b82d884d477
tobega <tobega>
2005-08-03 22:52:09 +0000

    Fixed a bug introduced by new float layout code

--- src/java/org/xhtmlrenderer/layout/inline/FloatUtil.java
+++ src/java/org/xhtmlrenderer/layout/inline/FloatUtil.java
@@ -19,7 +19,6 @@
  */
 package org.xhtmlrenderer.layout.inline;
 
-import java.awt.Rectangle;
 import org.xhtmlrenderer.css.constants.CSSName;
 import org.xhtmlrenderer.css.constants.IdentValue;
 import org.xhtmlrenderer.css.style.CalculatedStyle;
@@ -30,33 +29,38 @@ import org.xhtmlrenderer.layout.content.Content;
 import org.xhtmlrenderer.render.InlineBlockBox;
 import org.xhtmlrenderer.render.InlineBox;
 import org.xhtmlrenderer.render.LineBox;
-import org.xhtmlrenderer.util.XRRuntimeException;
 import org.xhtmlrenderer.util.Uu;
+import org.xhtmlrenderer.util.XRRuntimeException;
+
+import java.awt.*;
 
 
 /**
  * Description of the Class
  *
- * @author   Torbjörn Gannholm
+ * @author Torbjörn Gannholm
  */
 public class FloatUtil {
     /**
      * the new way of doing floats
      *
-     * @param c                PARAM
-     * @param prev_line        PARAM
-     * @param remaining_width  PARAM
-     * @return                 Returns
+     * @param c               PARAM
+     * @param prev_line       PARAM
+     * @param remaining_width PARAM
+     * @return Returns
      */
-    public static int adjustForTab( Context c, LineBox prev_line, int remaining_width ) {
-		if(prev_line.width == 0) {
-			prev_line.width = remaining_width;
-		} else {
-			Uu.p("warning. possible error. line already has width: " + prev_line);
-		}
+    public static int adjustForTab(Context c, LineBox prev_line, int remaining_width) {
+        if (prev_line.width == 0) {
+//temporarily set width as an "easy" way of passing this as parameter
+            prev_line.width = remaining_width;
+        } else {
+            Uu.p("warning. possible error. line already has width: " + prev_line);
+        }
         BlockFormattingContext bfc = c.getBlockFormattingContext();
-        remaining_width -= bfc.getLeftFloatDistance( prev_line );
-        remaining_width -= bfc.getRightFloatDistance( prev_line );
+        remaining_width -= bfc.getLeftFloatDistance(prev_line);
+        remaining_width -= bfc.getRightFloatDistance(prev_line);
+        //reset the line width to allow shrink wrap
+        prev_line.width = 0;
         //Uu.p("adjusting the line by: " + remaining_width);
         return remaining_width;
     }
@@ -66,37 +70,37 @@ public class FloatUtil {
     /**
      * Description of the Method
      *
-     * @param c          PARAM
+     * @param c         PARAM
      * @param content
-     * @param avail      PARAM
-     * @param curr_line  PARAM
-     * @return           Returns
+     * @param avail     PARAM
+     * @param curr_line PARAM
+     * @return Returns
      */
-    public static InlineBox generateFloatedBlockInlineBox( Context c, Content content, int avail, LineBox curr_line ) {
+    public static InlineBox generateFloatedBlockInlineBox(Context c, Content content, int avail, LineBox curr_line) {
         //Uu.p("generate floated block inline box: avail = " + avail);
         //Uu.p("generate floated block inline box");
         Rectangle oe = c.getExtents();// copy the extents for safety
-        c.setExtents( new Rectangle( oe ) );
+        c.setExtents(new Rectangle(oe));
 
         //BlockBox block = (BlockBox)layout.layout( c, (Element)node );
         InlineBlockBox inline_block = new InlineBlockBox();
         inline_block.element = content.getElement();
-        Boxing.layout( c, inline_block, content );
+        Boxing.layout(c, inline_block, content);
         
         //HACK: tobe 2004-12-22 - guessing here
         // calculate the float property
-        if ( c.getCurrentStyle().isIdent( CSSName.FLOAT, IdentValue.NONE ) ) {
-            throw new XRRuntimeException( "Invalid call to  generateFloatedBlockInlineBox(); where float: none " );
+        if (c.getCurrentStyle().isIdent(CSSName.FLOAT, IdentValue.NONE)) {
+            throw new XRRuntimeException("Invalid call to  generateFloatedBlockInlineBox(); where float: none ");
         }
 
         inline_block.floated = true;
 
-        IdentValue ident = c.getCurrentStyle().getIdent( CSSName.FLOAT );
-        if ( ident == IdentValue.LEFT ) {
+        IdentValue ident = c.getCurrentStyle().getIdent(CSSName.FLOAT);
+        if (ident == IdentValue.LEFT) {
             //inline_block.x = 0;
         }
 
-        if ( ident == IdentValue.RIGHT ) {
+        if (ident == IdentValue.RIGHT) {
             inline_block.x = oe.width - inline_block.width;
         }
         //HACK: tobe 2004-12-22 end
@@ -104,9 +108,9 @@ public class FloatUtil {
         inline_block.y = curr_line.y;
 
         //Uu.p("got a box now = : " + inline_block);
-        Rectangle bounds = new Rectangle( inline_block.x, inline_block.y,
-                inline_block.width, inline_block.height );
-        c.setExtents( oe );
+        Rectangle bounds = new Rectangle(inline_block.x, inline_block.y,
+                inline_block.width, inline_block.height);
+        c.setExtents(oe);
 
         //InlineBox box =
         // Uu.p("before newbox block = " + inline_block);
@@ -122,7 +126,7 @@ public class FloatUtil {
         inline_block.height = bounds.height;
         inline_block.break_after = false;
         inline_block.floated = true;
-        if ( inline_block.width > avail ) {
+        if (inline_block.width > avail) {
             inline_block.break_before = true;
         }
 
