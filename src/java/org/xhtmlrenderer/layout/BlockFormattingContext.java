a075e4ceaa5a2a635337e36565d4e1f7cd4ca70c
peterbrant <peterbrant>
2005-11-14 23:48:01 +0000

    Cleanup unused code

--- src/java/org/xhtmlrenderer/layout/BlockFormattingContext.java
+++ src/java/org/xhtmlrenderer/layout/BlockFormattingContext.java
@@ -4,13 +4,9 @@ import java.awt.Point;
 import java.util.List;
 
 import org.xhtmlrenderer.css.style.CssContext;
-import org.xhtmlrenderer.css.style.derived.RectPropertySet;
-import org.xhtmlrenderer.css.value.Border;
 import org.xhtmlrenderer.render.Box;
 import org.xhtmlrenderer.render.FloatedBlockBox;
 import org.xhtmlrenderer.render.LineBox;
-
-//TODO: refactor so that BFC utilizes master's contentWidth, etc.
 
 public class BlockFormattingContext {
     protected int x, y = 0;
@@ -24,35 +20,11 @@ public class BlockFormattingContext {
         persistentBFC = new PersistentBFC(block, c);
     }
 
-    public Border getInsets() {
-        return persistentBFC.insets;
-    }
-
-    public RectPropertySet getPadding() {
-        return persistentBFC.padding;
-    }
-
     /* ====== positioning stuff ======== */
-
-    public int getX() {
-        return persistentBFC.master.x + x;
-    }
-
-    public int getY() {
-        return persistentBFC.master.y + y;
-    }
 
     public Point getOffset() {
         //return new Point(x, y);
         return new Point(x, y);
-    }
-
-    public int getWidth() {
-        return persistentBFC.master.getWidth();
-    }
-
-    public int getHeight() {
-        return persistentBFC.master.height;
     }
 
     // we want to preserve the block formatting contexts position
@@ -94,10 +66,6 @@ public class BlockFormattingContext {
     }
     
     public String toString() {
-        return "BFC: (" + x + "," + y + ") - " + persistentBFC.master + "";
-    }
-
-    public Box getMaster() {
-        return persistentBFC.master;
+        return "BFC: (" + x + "," + y + ")";
     }
 }
--- src/java/org/xhtmlrenderer/layout/PersistentBFC.java
+++ src/java/org/xhtmlrenderer/layout/PersistentBFC.java
@@ -1,39 +1,11 @@
 package org.xhtmlrenderer.layout;
 
-import org.xhtmlrenderer.css.style.CalculatedStyle;
-import org.xhtmlrenderer.css.style.derived.BorderPropertySet;
-import org.xhtmlrenderer.css.style.derived.RectPropertySet;
-import org.xhtmlrenderer.css.value.Border;
 import org.xhtmlrenderer.render.Box;
 
-/**
- * Created by IntelliJ IDEA.
- * User: tobe
- * Date: 2005-okt-02
- * Time: 21:53:53
- * To change this template use File | Settings | File Templates.
- */
 public class PersistentBFC {
-    protected Box master = null;
-    
-    public Border insets;
-    public RectPropertySet padding;
-    
     private FloatManager floatManager = new FloatManager();
     
     public PersistentBFC(Box master, LayoutContext c) {
-        int parent_width = (int) c.getExtents().getWidth();
-        CalculatedStyle style = c.getCurrentStyle();
-        BorderPropertySet border = style.getBorder(c);
-        //note: percentages here refer to width of containing block
-        RectPropertySet margin = master.getStyle().getMarginWidth(c);
-        padding = style.getPaddingRect(parent_width, parent_width, c);
-        // CLEAN: cast to int
-        insets = new Border((int) margin.top() + (int) border.top() + (int) padding.top(),
-                (int) padding.right() + (int) border.right() + (int) margin.right(),
-                (int) padding.bottom() + (int) border.bottom() + (int) margin.bottom(),
-                (int) margin.left() + (int) border.left() + (int) padding.left());
-        this.master = master;
         master.setPersistentBFC(this);
         floatManager.setMaster(master);
     }
