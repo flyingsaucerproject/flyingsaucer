ea539087e230d1e67955a271a0160f6f6733ded1
peterbrant <peterbrant>
2005-11-28 22:26:52 +0000

    Implement CachingContent

--- src/java/org/xhtmlrenderer/layout/content/InlineBlockContent.java
+++ src/java/org/xhtmlrenderer/layout/content/InlineBlockContent.java
@@ -27,67 +27,32 @@ import java.util.List;
 
 
 /**
- * Represents the content of a replaced element (namespace-dependent)
- *
  * @author Torbjörn Gannholm
  */
-public class InlineBlockContent implements Content {
-    /**
-     * Description of the Field
-     */
+public class InlineBlockContent extends AbstractCachingContent implements Content {
     private Element _elem;
-    /**
-     * Description of the Field
-     */
+
     private CascadedStyle _style;
 
-    /**
-     * Constructor for the InlineBlockContent object
-     *
-     * @param e     PARAM
-     * @param style PARAM
-     */
     InlineBlockContent(Element e, CascadedStyle style) {
         _elem = e;
         _style = style;
     }
 
-    /**
-     * Converts to a String representation of the object.
-     *
-     * @return A string representation of the object.
-     */
     public String toString() {
         return "InlineBlock: " + _elem.getNodeName();
     }
 
-    /**
-     * Gets the element attribute of the InlineBlockContent object
-     *
-     * @return The element value
-     */
     public Element getElement() {
         return _elem;
     }
 
-    /**
-     * Gets the style attribute of the InlineBlockContent object
-     *
-     * @return The style value
-     */
     public CascadedStyle getStyle() {
         return _style;
     }
-
-    /**
-     * Gets the childContent attribute of the InlineBlockContent object
-     *
-     * @param c PARAM
-     * @return The childContent value
-     */
-    public List getChildContent(LayoutContext c) {
+    
+    protected List makeChildContent(LayoutContext c) {
         return ContentUtil.getChildContentList(c, this);
     }
-
 }
 
