95247ad12cc593ea31665e3f21b2e42784d466e0
pdoubleya <pdoubleya>
2006-07-28 10:09:11 +0000

    More accurate assignment of font sizes.

--- src/java/org/xhtmlrenderer/css/sheet/factory/FontSizeHackHelper.java
+++ src/java/org/xhtmlrenderer/css/sheet/factory/FontSizeHackHelper.java
@@ -1,15 +1,14 @@
 package org.xhtmlrenderer.css.sheet.factory;
 
+import org.w3c.dom.DOMException;
 import org.w3c.dom.css.CSSPrimitiveValue;
 import org.w3c.dom.css.Counter;
 import org.w3c.dom.css.RGBColor;
 import org.w3c.dom.css.Rect;
-import org.w3c.dom.DOMException;
-import org.w3c.css.sac.LexicalUnit;
 import org.xhtmlrenderer.css.value.FSCssValue;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,47 +22,54 @@ public class FontSizeHackHelper {
 
     static {
         // TODO:  there is a special cap on sizes too low; setting xx-small here to 50% of base, needs work
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("xx-small", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(50.0f)));
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("x-small", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(66.0f)));
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("small", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(66.0f)));
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("medium", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(100f)));
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("large", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(133f)));
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("x-large", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(166f)));
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("xx-large", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(200f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("xx-small", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(.75f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("x-small", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(0.8f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("small", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(.83f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("medium", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(1f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("large", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(1.17f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("x-large", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(1.5f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("xx-large", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(2f)));
 
         // HACK: absolutely no way to handle larger and smaller in this workaround; leave as a relative
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("larger", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(80.0f)));
-        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("smaller", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(120.0f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("larger", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(0.8f)));
+        FontSizeHackHelper.FONT_SIZE_ABS_CONVERT_MAP.put("smaller", new FSCssValue(new FontSizeHackHelper.FontSizeHackPrimitiveValue(1.2f)));
     }
 
-    /*package*/ static FSCssValue fontSizeAbsoluteHack(CSSPrimitiveValue primValue) {
+    /*package*/
+    static FSCssValue fontSizeAbsoluteHack(CSSPrimitiveValue primValue) {
         String val = primValue.getCssText();
-        FSCssValue fv = (FSCssValue) FONT_SIZE_ABS_CONVERT_MAP.get(val);
-        if ( fv == null ) {
-            return new FSCssValue(primValue);
-        } else {
-            return fv;
+
+        FSCssValue fv = null;
+        // TODO: in principle we should be able to check getPrimitiveType(), then only look in our map if it's an
+        // SAC_IDENT, but in testing the values returned did not match those declared in the interface, which indicates
+        // some version of SAC may be interposed (e.g. by the JVM). Hence we check the map every time, sorry!
+
+        short primitiveType = primValue.getPrimitiveType();
+        if ( primitiveType == CSSPrimitiveValue.CSS_IDENT ) {
+            fv = (FSCssValue) FONT_SIZE_ABS_CONVERT_MAP.get(val);
         }
+
+        if (fv == null) {
+            fv = new FSCssValue(primValue);
+        }
+        return fv;
     }
 
     static class FontSizeHackPrimitiveValue implements CSSPrimitiveValue {
-        private float pctValFloat;
+        private float floatValue;
         private String asString;
 
-        // HACK: absolute font sizes like "medium" are based on the user's preferred size
-        private static final int BASE_SIZE = 16;
-
-        public FontSizeHackPrimitiveValue(float pctVal) {
-            this.pctValFloat = Math.round(( pctVal / 100 ) * BASE_SIZE);
-            this.asString = "" + pctValFloat;
+        public FontSizeHackPrimitiveValue(float val) {
+            this.floatValue = val;
+            this.asString = String.valueOf(floatValue) + "em";
         }
 
         public short getPrimitiveType() {
-            return LexicalUnit.SAC_POINT;
+            return CSS_EMS;
         }
 
         public float getFloatValue(short unitType) throws DOMException {
-            return pctValFloat;
+            return floatValue;
         }
 
         public void setFloatValue(short unitType, float floatValue) throws DOMException {
