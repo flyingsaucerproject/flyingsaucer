c52a83cf47048e5cd85d22e105b78310fabb4e42
pdoubleya <pdoubleya>
2006-08-06 20:21:19 +0000

    Use relative paths

--- www/r6/guide.bat
+++ www/r6/guide.bat
@@ -1,3 +1,3 @@
 del users-guide-r6.pdf
-java -cp \build\classes;\lib\cssparser-0-9-4-fs.jar;\lib\itext-paulo-155.jar org.xhtmlrenderer.simple.PDFRenderer d:\java\javanet\xhtmlrenderer\www\r6\users-guide-r6.html users-guide-r6.pdf
+java -cp ..\..\build\classes;..\..\lib\cssparser-0-9-4-fs.jar;..\..\lib\itext-paulo-155.jar org.xhtmlrenderer.simple.PDFRenderer d:\java\javanet\xhtmlrenderer\www\r6\users-guide-r6.html users-guide-r6.pdf
 start users-guide-r6.pdf
