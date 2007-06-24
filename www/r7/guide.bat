1839adda2365de61ff9998c8e10ad77fd39a3876
pdoubleya <pdoubleya>
2007-06-24 22:04:36 +0000

    First pass, copy files from R6, replace references to version.

---
+++ www/r7/guide.bat
@@ -0,0 +1,3 @@
+del users-guide-r7.pdf
+java -cp ..\..\build\classes;..\..\lib\cssparser-0-9-4-fs.jar;..\..\lib\itext-paulo-155.jar org.xhtmlrenderer.simple.PDFRenderer d:\java\javanet\xhtmlrenderer\www\r7\users-guide-r7.html users-guide-r7.pdf
+start users-guide-r7.pdf
