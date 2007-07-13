b4fcd4d2280a72e6c2b30b1a84a62180a4658869
pdoubleya <pdoubleya>
2007-07-13 13:42:54 +0000

    remove ref to depr. method

--- demos/samples/src/ImageRender.java
+++ demos/samples/src/ImageRender.java
@@ -38,7 +38,7 @@ public class ImageRender {
 				imageWriter.write(image, path + ".png");
 
 				// compare to old
-				BufferedImage img = Graphics2DRenderer.renderToImage(f.toURL().toExternalForm(), 1024);
+				BufferedImage img = Graphics2DRenderer.renderToImage(f.toURI().toURL().toExternalForm(), 1024);
 				ImageIO.write(img, "png", new File(path + "-G2DR.png"));
 
 			} else {
