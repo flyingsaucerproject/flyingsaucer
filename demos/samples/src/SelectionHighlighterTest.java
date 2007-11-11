98c2f842695b0a6ec999ae3f5e79f80807d3181f
nitsk <nitsk>
2007-11-11 12:49:37 +0000

    Fix copyright

--- demos/samples/src/SelectionHighlighterTest.java
+++ demos/samples/src/SelectionHighlighterTest.java
@@ -1,6 +1,6 @@
 /*
  * {{{ header & license
- * Copyright (c) 2007 Wisconsin Court System
+ * Copyright (c) 2007 Nick Reddel
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
