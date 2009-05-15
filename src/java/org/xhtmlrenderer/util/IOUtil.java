79c0371b07ead4224aaede31e8635b838dd977b6
pdoubleya <pdoubleya>
2009-05-15 15:45:52 +0000

    Add new helper method for reading from a URL

--- src/java/org/xhtmlrenderer/util/IOUtil.java
+++ src/java/org/xhtmlrenderer/util/IOUtil.java
@@ -1,6 +1,8 @@
 package org.xhtmlrenderer.util;
 
 import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @author patrick
@@ -51,5 +53,43 @@ public class IOUtil {
                 throw new IOException("Cleanup directory " + dir + ", can't delete file " + file);
             }
         }
+    }
+
+    /**
+     * Attempts to open a connection, and a stream, to the URI provided. timeouts will be set for opening the connection
+     * and reading from it. will return the stream, or null if unable to open or read or a timeout occurred. Does not
+     * buffer the stream.
+     */
+    public static InputStream openStreamAtUrl(String uri) {
+        InputStream is = null;
+        try {
+            final URLConnection uc = new URL(uri).openConnection();
+
+            // If using Java 5+ you can set timeouts for the URL connection--useful if the remote
+            // server is down etc.; the default timeout is pretty long
+            //
+            //uc.setConnectTimeout(10 * 1000);
+            //uc.setReadTimeout(30 * 1000);
+            //
+            // TODO:CLEAN-JDK1.4
+            // Since we target 1.4, we use a couple of system properties--note these are only supported
+            // in the Sun JDK implementation--see the Net properties guide in the JDK
+            // e.g. file:///usr/java/j2sdk1.4.2_17/docs/guide/net/properties.html
+            System.setProperty("sun.net.client.defaultConnectTimeout", String.valueOf(10 * 1000));
+            System.setProperty("sun.net.client.defaultReadTimeout", String.valueOf(30 * 1000));
+
+            uc.connect();
+
+            is = uc.getInputStream();
+        } catch (java.net.MalformedURLException e) {
+            XRLog.exception("bad URL given: " + uri, e);
+        } catch (FileNotFoundException e) {
+            XRLog.exception("item at URI " + uri + " not found");
+        } catch (IOException e) {
+            XRLog.exception("IO problem for " + uri, e);
+        }
+
+        return is;
+
     }
 }
