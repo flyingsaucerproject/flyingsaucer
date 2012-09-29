8ae8875b67cf1906503d08545ec1adc20abdc580
Jan Habermann <jan@habermann24.com>
2012-09-29 17:43:51 +0200

    Added setOpacity to the output device. Implemented basic opacity and rgba-color support in PDF outputdevice.

--- flying-saucer-core/src/main/java/org/xhtmlrenderer/css/constants/CSSName.java
+++ flying-saucer-core/src/main/java/org/xhtmlrenderer/css/constants/CSSName.java
@@ -757,7 +757,7 @@ public final class CSSName implements Comparable {
     		addProperty(
     				"opacity",
     				PRIMITIVE,
-    				"0",
+    				"1",
     				INHERITS,
     				true,
     				new PrimitivePropertyBuilders.Opacity()
