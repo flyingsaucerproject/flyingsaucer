975106d0d7b504dff521e0bb367310e13eb35256
Thomas Geese <thomas.geese@it-tecture.de>
2015-02-19 11:19:45 +0100

    Refactor the opening of a input stream to handle redirection with HttpURLConnection.

--- flying-saucer-core/src/main/java/org/xhtmlrenderer/swing/NaiveUserAgent.java
+++ flying-saucer-core/src/main/java/org/xhtmlrenderer/swing/NaiveUserAgent.java
@@ -26,14 +26,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 
 import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
 
 import org.xhtmlrenderer.event.DocumentListener;
 import org.xhtmlrenderer.extend.UserAgentCallback;
@@ -133,9 +136,68 @@ public class NaiveUserAgent implements UserAgentCallback, DocumentListener {
     }
 	
 	protected InputStream openStream(String uri) throws MalformedURLException, IOException {
-		return new URL(uri).openStream();
+		return openConnection(uri).getInputStream();
 	}
 
+	/**
+	 * Opens a connections to uri.
+	 * 
+	 * This can be overwritten to customize handling of connections by type.
+	 * 
+	 * @param uri the uri to connect to
+	 * @return URLConnection opened connection to uri
+	 * @throws IOException if an I/O exception occurs.
+	 */
+	protected URLConnection openConnection(String uri) throws IOException {
+		URLConnection connection = new URL(uri).openConnection();
+		if (connection instanceof HttpURLConnection) {
+			connection = onHttpConnection((HttpURLConnection) connection);
+		}
+		return connection;
+	}
+
+	/**
+	 * Customized handling of @link{HttpUrlConnection}.
+	 * 
+	 * 
+	 * @param origin the original connection
+	 * @return @link{URLConnection} 
+	 * 
+	 * @throws MalformedURLException if an unknown protocol is specified.
+	 * @throws IOException if an I/O exception occurs.
+	 */
+	protected URLConnection onHttpConnection(HttpURLConnection origin) throws MalformedURLException, IOException {
+		URLConnection connection = origin;
+		int status = origin.getResponseCode();
+
+		if (needsRedirect(status)) {
+			// get redirect url from "location" header field
+			String newUrl = origin.getHeaderField("Location");
+			XRLog.load("Connection is redirected to: " + newUrl);
+			// open the new connnection again
+			connection = new URL(newUrl).openConnection();
+		}
+		return connection;
+	}
+
+	/**
+	 * Verify that return code of connection represents a redirection.
+	 * 
+	 * But it is final because redirection processing is determined.
+	 * 
+	 * @param status return code of connection
+	 * @return boolean true if return code is a 3xx
+	 */
+	protected final boolean needsRedirect(int status) {
+		return 
+				status != HttpURLConnection.HTTP_OK
+				&& (
+					status == HttpURLConnection.HTTP_MOVED_TEMP
+					|| status == HttpURLConnection.HTTP_MOVED_PERM
+					|| status == HttpURLConnection.HTTP_SEE_OTHER
+				);
+	}
+	
     /**
      * Retrieves the CSS located at the given URI.  It's assumed the URI does point to a CSS file--the URI will
      * be accessed (using java.io or java.net), opened, read and then passed into the CSS parser.
