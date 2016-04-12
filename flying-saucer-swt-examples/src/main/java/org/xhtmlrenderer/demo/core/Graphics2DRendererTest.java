b220afee85a0a37641313f650f094748cef031e6
infi.he <infi.he@ndpmedia.com>
2016-04-12 14:39:00 +0800

    @font-face bug

---
+++ flying-saucer-swt-examples/src/main/java/org/xhtmlrenderer/demo/core/Graphics2DRendererTest.java
@@ -0,0 +1,19 @@
+package org.xhtmlrenderer.demo.core;
+
+import org.xhtmlrenderer.simple.Graphics2DRenderer;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * Created by infi.he on 2016/4/12.
+ */
+public class Graphics2DRendererTest {
+
+    public static void main(String[] args)  throws Exception {
+        BufferedImage img = Graphics2DRenderer.renderToImageAutoSize(new File("demos/fontface/fontface.html").toURL().toExternalForm(),
+                700, BufferedImage.TYPE_INT_ARGB);
+        ImageIO.write(img, "png", new File("test.png"));
+    }
+}
