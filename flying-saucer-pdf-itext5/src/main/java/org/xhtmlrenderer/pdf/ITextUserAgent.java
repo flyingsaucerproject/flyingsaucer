flyingsaucerproject/flyingsaucer

commit 600b692f6ccc033bdb44ab28ceae1216aeed476e

Cache data: URI images
Use single source for all same image instances.

flying-saucer-pdf-itext5/src/main/java/org/xhtmlrenderer/pdf/ITextUserAgent.java
@@ -24,7 +24,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URL;
 
 import org.xhtmlrenderer.extend.FSImage;
 import org.xhtmlrenderer.layout.SharedContext;
@@ -63,13 +62,17 @@ public class ITextUserAgent extends NaiveUserAgent {
     }
 
     public ImageResource getImageResource(String uriStr) {
-        ImageResource resource = null;
-        if (ImageUtil.isEmbeddedBase64Image(uriStr)) {
-            resource = loadEmbeddedBase64ImageResource(uriStr);
-        } else {
+        ImageResource resource;
+        if (!ImageUtil.isEmbeddedBase64Image(uriStr)) {
             uriStr = resolveURI(uriStr);
-            resource = (ImageResource) _imageCache.get(uriStr);
-            if (resource == null) {
+        }
+        resource = (ImageResource) _imageCache.get(uriStr);
+
+        if (resource == null) {
+            if (ImageUtil.isEmbeddedBase64Image(uriStr)) {
+                resource = loadEmbeddedBase64ImageResource(uriStr);
+                _imageCache.put(uriStr, resource);
+            } else {
                 InputStream is = resolveAndOpenStream(uriStr);
                 if (is != null) {
                     try {

flying-saucer-pdf/src/main/java/org/xhtmlrenderer/pdf/ITextUserAgent.java
@@ -24,7 +24,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URL;
 
 import org.xhtmlrenderer.extend.FSImage;
 import org.xhtmlrenderer.layout.SharedContext;
@@ -62,13 +61,17 @@ public class ITextUserAgent extends NaiveUserAgent {
     }
 
     public ImageResource getImageResource(String uriStr) {
-        ImageResource resource = null;
-        if (ImageUtil.isEmbeddedBase64Image(uriStr)) {
-            resource = loadEmbeddedBase64ImageResource(uriStr);
-        } else {
+        ImageResource resource;
+        if (!ImageUtil.isEmbeddedBase64Image(uriStr)) {
             uriStr = resolveURI(uriStr);
-            resource = (ImageResource) _imageCache.get(uriStr);
-            if (resource == null) {
+        }
+        resource = (ImageResource) _imageCache.get(uriStr);
+
+        if (resource == null) {
+            if (ImageUtil.isEmbeddedBase64Image(uriStr)) {
+                resource = loadEmbeddedBase64ImageResource(uriStr);
+                _imageCache.put(uriStr, resource);
+            } else {
                 InputStream is = resolveAndOpenStream(uriStr);
                 if (is != null) {
                     try {

commit dc6a75c904fab6c535351ece9e1d0e1c1e8eaf24

Always clone images returned from cache
See 139afbb559d51386acf7dc45df448ef062898b59

Regression from 600b692f6ccc033bdb44ab28ceae1216aeed476e / #100

flying-saucer-pdf-itext5/src/main/java/org/xhtmlrenderer/pdf/ITextUserAgent.java
@@ -104,16 +104,15 @@ public class ITextUserAgent extends NaiveUserAgent {
                     }
                 }
             }
-
-            if (resource != null) {
-                FSImage image=resource.getImage();
-                if (image instanceof ITextFSImage) {
-                    image=(FSImage) ((ITextFSImage) resource.getImage()).clone();
-                }
-                resource = new ImageResource(resource.getImageUri(), image);
-            } else {
-                resource = new ImageResource(uriStr, null);
+        }
+        if (resource != null) {
+            FSImage image = resource.getImage();
+            if (image instanceof ITextFSImage) {
+                image = (FSImage) ((ITextFSImage) resource.getImage()).clone();
             }
+            resource = new ImageResource(resource.getImageUri(), image);
+        } else {
+            resource = new ImageResource(uriStr, null);
         }
         return resource;
     }

flying-saucer-pdf/src/main/java/org/xhtmlrenderer/pdf/ITextUserAgent.java
@@ -103,16 +103,15 @@ public class ITextUserAgent extends NaiveUserAgent {
                     }
                 }
             }
-
-            if (resource != null) {
-                FSImage image=resource.getImage();
-                if (image instanceof ITextFSImage) {
-                    image=(FSImage) ((ITextFSImage) resource.getImage()).clone();
-                }
-                resource = new ImageResource(resource.getImageUri(), image);
-            } else {
-                resource = new ImageResource(uriStr, null);
+        }
+        if (resource != null) {
+            FSImage image = resource.getImage();
+            if (image instanceof ITextFSImage) {
+                image = (FSImage) ((ITextFSImage) resource.getImage()).clone();
             }
+            resource = new ImageResource(resource.getImageUri(), image);
+        } else {
+            resource = new ImageResource(uriStr, null);
         }
         return resource;
     }