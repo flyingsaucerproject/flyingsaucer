4778f9eefd06cc393c5978984cd60a253987f5f7
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-05-01 14:14:53 +0300

    #318 make the test failure easily readable
    We first compose a list of font names, and then assert that this list contains the expected value. In this case, the test generates a readable error message when it fails:
    
    java.lang.AssertionError: [Should contain Jacquard24 font]
    Expecting any elements of:
      ["Courier-Bold", "Helvetica"]
    to match given predicate but none did.

--- flying-saucer-pdf/src/test/java/org/xhtmlrenderer/pdf/CssFontFaceTest.java
+++ flying-saucer-pdf/src/test/java/org/xhtmlrenderer/pdf/CssFontFaceTest.java
@@ -1,47 +1,56 @@
 package org.xhtmlrenderer.pdf;
 
-import static com.codeborne.pdftest.assertj.Assertions.assertThat;
-import static java.util.Objects.requireNonNull;
-
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDResources;
-import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static com.codeborne.pdftest.assertj.Assertions.assertThat;
+import static java.util.Objects.requireNonNull;
 
 public class CssFontFaceTest {
 
-  /**
-   * Verifies that font-face declarations with the -fs-pdf-font-embed property
-   * actually results in the font being embedded in the pdf
-   *
-   * The Jacquard24 font used to test is from <a href="https://fonts.google.com/specimen/Jacquard+24">Google Fonts</a>
-   * and is licenced under the Open Font License
-   *
-   * @throws IOException is any of resources fails to load
-   */
-  @Test
-  public void testAutoInstallationOfCssDeclaredFonts() throws IOException {
-    URL htmlUrl = requireNonNull(getClass().getResource("fonts/CssFontFace.html"),
-        "test resource not found: fonts/CssFontFace.html");
-    byte[] pdfBytes = Html2Pdf.fromUrl(htmlUrl);
-    try (PDDocument document = PDDocument.load(pdfBytes)) {
-      boolean hasEmbeddedFont = false;
-      for (int i = 0; i < document.getNumberOfPages(); ++i) {
-        PDPage page = document.getPage(i);
-        PDResources res = page.getResources();
-        for (COSName fontName : res.getFontNames()) {
-          PDFont font = res.getFont(fontName);
-          if (font.getFontDescriptor().getFontName().contains("Jacquard24")) {
-            hasEmbeddedFont = true;
-          }
+    /**
+     * Verifies that font-face declarations with the -fs-pdf-font-embed property
+     * actually results in the font being embedded in the pdf
+     * <p>
+     * The Jacquard24 font used to test is from <a href="https://fonts.google.com/specimen/Jacquard+24">Google Fonts</a>
+     * and is licenced under the Open Font License
+     */
+    @Test
+    public void autoInstallationOfCssDeclaredFonts() throws IOException {
+        URL htmlUrl = requireNonNull(getClass().getResource("fonts/CssFontFace.html"), "test resource not found: fonts/CssFontFace.html");
+        byte[] pdfBytes = Html2Pdf.fromUrl(htmlUrl);
+        try (PDDocument document = PDDocument.load(pdfBytes)) {
+            assertThat(document.getNumberOfPages()).isGreaterThanOrEqualTo(1);
+            for (int i = 0; i < document.getNumberOfPages(); ++i) {
+                PDPage page = document.getPage(i);
+                PDResources res = page.getResources();
+                List<String> fontNames = getFontNames(res);
+                assertThat(fontNames)
+                        .as("Should contain Jacquard24 font")
+                        .anyMatch(name -> name.contains("Jacquard24"));
+            }
         }
-      }
-      assertThat(hasEmbeddedFont).isTrue();
     }
-  }
+
+    private List<String> getFontNames(PDResources res) {
+        return StreamSupport.stream(res.getFontNames().spliterator(), false)
+                .map(font -> getFontName(res, font))
+                .toList();
+    }
+
+    private String getFontName(PDResources res, COSName font) {
+        try {
+            return res.getFont(font).getFontDescriptor().getFontName();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
