a058a088c04b89814827c1af4fcc3cbd7448ea2d
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-10-30 21:09:36 +0200

    #429 [refactor] make class `PaintingInfo` immutable

--- flying-saucer-core/src/main/java/org/xhtmlrenderer/layout/PaintingInfo.java
+++ flying-saucer-core/src/main/java/org/xhtmlrenderer/layout/PaintingInfo.java
@@ -19,8 +19,7 @@
  */
 package org.xhtmlrenderer.layout;
 
-import java.awt.Dimension;
-import java.awt.Rectangle;
+import java.awt.*;
 
 /**
  * A bean which every box uses to provide its aggregate bounds (which may be
@@ -30,34 +29,26 @@ import java.awt.Rectangle;
  * overflow property into account.
  */
 public class PaintingInfo {
-    private Dimension _outerMarginCorner;
-    private Rectangle _aggregateBounds;
+    private final Dimension _outerMarginCorner;
+    private final Rectangle _aggregateBounds;
 
-    public PaintingInfo() {
+    public PaintingInfo(Dimension outerMarginCorner, Rectangle aggregateBounds) {
+        _outerMarginCorner = outerMarginCorner;
+        _aggregateBounds = aggregateBounds;
     }
 
     public Rectangle getAggregateBounds() {
         return _aggregateBounds;
     }
 
-    public void setAggregateBounds(Rectangle aggregateBounds) {
-        _aggregateBounds = aggregateBounds;
-    }
-
     public Dimension getOuterMarginCorner() {
         return _outerMarginCorner;
     }
 
-    public void setOuterMarginCorner(Dimension outerMarginCorner) {
-        _outerMarginCorner = outerMarginCorner;
-    }
-
     public PaintingInfo copyOf() {
-        PaintingInfo result = new PaintingInfo();
-        result.setOuterMarginCorner(new Dimension(_outerMarginCorner));
-        result.setAggregateBounds(new Rectangle(_aggregateBounds));
-
-        return result;
+        return new PaintingInfo(
+                new Dimension(_outerMarginCorner), new Rectangle(_aggregateBounds)
+        );
     }
 
     public void translate(int tx, int ty) {
