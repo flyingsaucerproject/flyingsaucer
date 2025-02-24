05caf10263706d87e05b5bb7b9ac6999a4855056
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-02-24 19:28:32 +0200

    move sample.html to module "flying-saucer-pdf"
    this make possible using helper method "printFile" to log and check generated PDF file.

--- flying-saucer-pdf/src/test/java/org/xhtmlrenderer/pdf/ConcurrentPdfGenerationTest.java
+++ flying-saucer-pdf/src/test/java/org/xhtmlrenderer/pdf/ConcurrentPdfGenerationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,13 +19,15 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.xhtmlrenderer.pdf.TestUtils.printFile;
 
 public class ConcurrentPdfGenerationTest {
     private static final Logger log = LoggerFactory.getLogger(ConcurrentPdfGenerationTest.class);
 
     @Test
-    public void samplePdf() {
-        byte[] pdf = generatePdf("sample.html");
+    public void samplePdf() throws IOException {
+        byte[] bytes = generatePdf("sample.html");
+        PDF pdf = printFile(log, bytes, "sample.pdf");
         verifyPdf(pdf);
     }
 
@@ -34,13 +37,13 @@ public class ConcurrentPdfGenerationTest {
         timer.scheduleWithFixedDelay(System::gc, 0, 15, MILLISECONDS);
 
         List<Throwable> failures = new ArrayList<>();
-        ExecutorService pool = newFixedThreadPool(10);
-        for (int j = 0; j < 200; j++) {
+        ExecutorService pool = newFixedThreadPool(4);
+        for (int j = 0; j < 20; j++) {
             final int i = j;
             pool.submit(() -> {
                 try {
                     byte[] pdf = generatePdf("sample.html");
-                    verifyPdf(pdf);
+                    verifyPdf(new PDF(pdf));
                     log.info("Check #{} ok", i);
                 }
                 catch (Throwable e) {
@@ -52,12 +55,11 @@ public class ConcurrentPdfGenerationTest {
 
         pool.shutdown();
         assert pool.awaitTermination(250, SECONDS) : "Timeout!";
-        
+
         assertThat(failures).isEmpty();
     }
 
-    private void verifyPdf(byte[] pdfBytes) {
-        PDF pdf = new PDF(pdfBytes);
+    private void verifyPdf(PDF pdf) {
         assertThat(pdf).containsText("Bill To:", "John doe", "john.do@mail.com");
         assertThat(pdf).containsText("Invoice #:", "INV-666");
         assertThat(pdf).containsText("Invoice Date:", "Sep 27, 2023");
@@ -65,9 +67,8 @@ public class ConcurrentPdfGenerationTest {
     }
 
     private byte[] generatePdf(String htmlPath) {
-        URL htmlUrl = requireNonNull(currentThread().getContextClassLoader().getResource(htmlPath), 
+        URL htmlUrl = requireNonNull(currentThread().getContextClassLoader().getResource(htmlPath),
                 () -> "Test resource not found: " + htmlPath);
         return Html2Pdf.fromUrl(htmlUrl);
     }
-
 }
