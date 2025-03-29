069e0e581ea4633b2544e486cb1ce1332b7d0330
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-03-29 17:29:44 +0200

    #481 [refactor] hold float+boolean in a separate object
    it will allow to simplify code: calculate and return this object from a helper method.

--- flying-saucer-core/src/main/java/org/xhtmlrenderer/css/style/BorderRadiusCorner.java
+++ flying-saucer-core/src/main/java/org/xhtmlrenderer/css/style/BorderRadiusCorner.java
@@ -8,111 +8,95 @@ import org.xhtmlrenderer.css.style.derived.ListValue;
 
 public class BorderRadiusCorner {
 
-    public static final BorderRadiusCorner UNDEFINED = new BorderRadiusCorner();
+    public static final BorderRadiusCorner UNDEFINED = new BorderRadiusCorner(0, 0);
 
-    private boolean _leftPercent = false;
-    private boolean _rightPercent = false;
-    private float _left;
-    private float _right;
+    private record Length(float value, boolean percent) {}
+
+    private Length _left;
+    private Length _right;
 
     // TODO: FIXME the way values are passed from the CSS to the border corners really sucks, improve it
 
-    public BorderRadiusCorner() {
-    }
     public BorderRadiusCorner(float left, float right) {
-        this._left = left;
-        this._right = right;
+        this._left = new Length(left, false);
+        this._right = new Length(right, false);
     }
+
     public BorderRadiusCorner(CSSName fromVal, CalculatedStyle style, CssContext ctx) {
         FSDerivedValue value = style.valueByName(fromVal);
         if (value instanceof ListValue lValues) {
-            PropertyValue first = (PropertyValue)lValues.getValues().get(0);
-            PropertyValue second = lValues.getValues().size() > 1 ?
-                    (PropertyValue) lValues.getValues().get(1) : first;
+            PropertyValue first = (PropertyValue) lValues.getValues().get(0);
+            PropertyValue second = lValues.getValues().size() > 1 ? (PropertyValue) lValues.getValues().get(1) : first;
 
             if (fromVal.equals(CSSName.BORDER_TOP_LEFT_RADIUS) || fromVal.equals(CSSName.BORDER_BOTTOM_RIGHT_RADIUS)) {
                 setRight(fromVal, style, first, ctx);
                 setLeft(fromVal, style, second, ctx);
-                //_left = style.getFloatPropertyProportionalHeight(fromVal, 0, ctx);
-                //_right = style.getFloatPropertyProportionalWidth(fromVal, 0, ctx);
             } else {
                 setLeft(fromVal, style, first, ctx);
                 setRight(fromVal, style, second, ctx);
-                //_right = style.getFloatPropertyProportionalHeight(fromVal, 0, ctx);
-                //_left = style.getFloatPropertyProportionalWidth(fromVal, 0, ctx);
             }
-        } else if (value instanceof LengthValue lv){
+        } else if (value instanceof LengthValue lv) {
 
-            if(lv.getStringValue().contains("%")) {
-                _leftPercent = _rightPercent = true;
-                _left = _right = value.asFloat() / 100.0f;
+            if (lv.getStringValue().contains("%")) {
+                _left = _right = new Length(value.asFloat() / 100.0f, true);
             } else {
-                _left = _right = (int) lv.getFloatProportionalTo(fromVal, 0, ctx);
+                _left = _right = new Length((int) lv.getFloatProportionalTo(fromVal, 0, ctx), false);
             }
-            //first = second = (LengthValue)value;
         }
-            //System.out.println(fromVal + " " + _left + " " + _right);
-            //this._right = this._left = ((PropertyValue)((ListValue)fromVal).getValues().get(0)).getFloatValue();
-        /*} else if(fromVal instanceof LengthValue) {
-            this._left = fromVal.asFloat();
-            this._right = fromVal.asFloat();
-        }*/
-
     }
 
     private void setLeft(CSSName fromVal, CalculatedStyle style, PropertyValue value, CssContext ctx) {
         if (value.getPrimitiveType() == CSSPrimitiveValue.CSS_PERCENTAGE) {
-            _leftPercent = true;
-            _left = value.getFloatValue() / 100.0f;
+            _left = new Length(value.getFloatValue() / 100.0f, true);
         } else {
-            _left = (int)LengthValue.calcFloatProportionalValue(
-                    style,
-                    fromVal,
-                    value.getCssText(),
-                    value.getFloatValue(),
-                    value.getPrimitiveType(),
-                    0,
-                    ctx);
+            _left = new Length((int) LengthValue.calcFloatProportionalValue(
+                style,
+                fromVal,
+                value.getCssText(),
+                value.getFloatValue(),
+                value.getPrimitiveType(),
+                0,
+                ctx), false);
         }
     }
+
     private void setRight(CSSName fromVal, CalculatedStyle style, PropertyValue value, CssContext ctx) {
         if (value.getPrimitiveType() == CSSPrimitiveValue.CSS_PERCENTAGE) {
-            _rightPercent = true;
-            _right = value.getFloatValue() / 100.0f;
+            _right = new Length(value.getFloatValue() / 100.0f, true);
         } else {
-            _right = (int)LengthValue.calcFloatProportionalValue(
-                    style,
-                    fromVal,
-                    value.getCssText(),
-                    value.getFloatValue(),
-                    value.getPrimitiveType(),
-                    0,
-                    ctx);
+            _right = new Length((int) LengthValue.calcFloatProportionalValue(
+                style,
+                fromVal,
+                value.getCssText(),
+                value.getFloatValue(),
+                value.getPrimitiveType(),
+                0,
+                ctx), false);
         }
     }
 
     public boolean hasRadius() {
-        return _left > 0 || _right > 0;
+        return _left.value() > 0 || _right.value() > 0;
     }
 
     public float getMaxLeft(float max) {
-        if (_leftPercent)
-            return max * _left;
-        return Math.min(_left, max);
+        if (_left.percent())
+            return max * _left.value();
+        return Math.min(_left.value(), max);
     }
 
     public float getMaxRight(float max) {
-        if (_rightPercent)
-            return max * _right;
-        return Math.min(_right, max);
+        if (_right.percent())
+            return max * _right.value();
+        return Math.min(_right.value(), max);
     }
 
 
     public float left() {
-        return _left;
-    }
-    public float right() {
-        return _right;
+        return _left.value();
     }
 
+    public float right() {
+        return _right.value();
+    }
 }
