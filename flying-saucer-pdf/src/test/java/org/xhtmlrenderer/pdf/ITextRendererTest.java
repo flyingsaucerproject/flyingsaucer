973c8a4a91d2ee7e0b05cf48908a5521a4f99b67
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-08-24 23:26:17 +0300

    [refactor] split ITextRendererTest to multiple independent tests

--- flying-saucer-pdf/src/test/java/org/xhtmlrenderer/pdf/ITextRendererTest.java
+++ flying-saucer-pdf/src/test/java/org/xhtmlrenderer/pdf/ITextRendererTest.java
@@ -3,25 +3,34 @@ package org.xhtmlrenderer.pdf;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ITextRendererTest {
+    private final ITextRenderer cut = new ITextRenderer();
+
+    @Test
+    void versionIsNullByDefault() {
+        assertThat(cut.getPDFVersion()).isNull();
+    }
 
     @Test
     void getAndSetPDFVersion() {
-
-        var cut = new ITextRenderer();
-
-        assertThat(cut.getPDFVersion()).isNull();
-
         cut.setPDFVersion("2.0");
         assertThat(cut.getPDFVersion()).isEqualTo("2.0");
+    }
 
-        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> cut.setPDFVersion("0.1"));
-
-        assertThat(thrown.getMessage()).isEqualTo("Invalid PDF version character: \"0.1\"; use one of constants in [1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 2.0].");
-
+    @Test
+    void canSetVersionNull() {
         cut.setPDFVersion(null);
         assertThat(cut.getPDFVersion()).isEqualTo(null);
+    }
+
+    @Test
+    void cannotSetIllegalVersion() {
+        assertThatThrownBy(() -> cut.setPDFVersion("0.1"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("""
+                Invalid PDF version character: "0.1"; use one of constants in [1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 2.0].
+                """.trim());
     }
 }
